b9838a13aa6e13d79c6120fbc5db0826fec4bc06
P2673516 <P2673516@my365.dmu.ac.uk>
2023-11-16 22:42:21 +0000

    um

--- Application_Clean/src/Polight.cpp
+++ Application_Clean/src/Polight.cpp
@@ -1,15 +1,15 @@
-//#include "Polight.h"
-//
-//Polight::Polight(glm::vec3 col, glm::vec3 pos, glm::vec3 constants) : m_colour(col), m_pos(pos), m_const(constants)
-//{
-//	
-//}
-//
-//
-//void Polight::setLU(Shader* shader)
-//{
-//	shader->use();
-//	shader->setVec3("plightpos", m_pos);
-//	shader->setVec3("plightcl", m_colour);
-//	shader->setVec3("plight[0].pAtt", m_const);
-//}
+#include "Polight.h"
+
+Polight::Polight(glm::vec3 col, glm::vec3 pos, glm::vec3 constants) : m_colour(col), m_pos(pos), m_const(constants)
+{
+	
+}
+
+
+void Polight::setLU(Shader* shader)
+{
+	shader->use();
+	shader->setVec3("plightpos", m_pos);
+	shader->setVec3("plightcl", m_colour);
+	shader->setVec3("plight[0].pAtt", m_const);
+}
